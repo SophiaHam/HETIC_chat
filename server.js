495a9b99b80629b928a71195c10310470e69c2cb
Sophia Hamiteche <sophiahamiteche@MacBook-Pro-de-Sophia.local>
2019-02-14 11:32:56 +0100

    Fichier server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 Imports
 */
 
+// => Créer des constantes pour importer les modules NPM
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
 /*
 Configuration

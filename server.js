c6f52d32faeeb8dcea9b0e208ec6137ee37488f5
Sophia Hamiteche <sophiahamiteche@MacBook-Pro-de-Sophia.local>
2019-02-14 12:10:35 +0100

    Fichier server.js

--- server.js
+++ server.js
@@ -2,17 +2,31 @@
 Imports
 */
 
+// => Variable d'environnement
+require('dotenv').config()
+
 // => Créer des constantes pour importer les modules NPM
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const ejs = require('ejs');
+
+
 
 /*
 Configuration
 */
+
+const server = express();
+const port = process.env.PORT;
 
 
 
 /*
 Start
 */
+
+//Lancer le serveur
+server.listen(port, () => {
+    console.log({server:`Server listening on port ${port}`});
+})
